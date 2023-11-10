4dd33140954a660dc6e5aa90f1908cda1b7938f6
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2023-11-10 20:12:09 +0530

    new commit

---
+++ Modern-Chair-Store/Assingment.js
@@ -0,0 +1,12 @@
+// let arr = [1 , 2, 5, 6, 8];
+// function greaterNum(num){
+//     for (let i = 0 ;i<arr.length;i++){
+//         if(arr[i] > num){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+
+// greaterNum(4);
+
+N
